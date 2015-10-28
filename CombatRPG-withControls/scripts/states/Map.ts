9fd2483ed67a89810007372caa6f3faad7da1902
Jesse Glover <kohouri@gmail.com>
2015-10-28 01:28:36 -0400

    Spritesheet

--- CombatRPG-withControls/scripts/states/Map.ts
+++ CombatRPG-withControls/scripts/states/Map.ts
@@ -1,7 +1,12 @@
 ﻿namespace CombatRPG {
     export namespace States {
         export class Map extends Phaser.State {
-
+            preload() {
+                this.load.spritesheet("character", "/assets/images/character.png", 64, 64);
+            }
+            create() {
+                
+            }
         }
     }
 }
