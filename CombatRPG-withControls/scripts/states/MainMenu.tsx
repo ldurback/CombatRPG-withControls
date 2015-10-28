75ecbb73f3b73ad2270efb970612d032c7897fe9
Jesse Glover <kohouri@gmail.com>
2015-10-28 01:05:07 -0400

    loplo

--- CombatRPG-withControls/scripts/states/MainMenu.tsx
+++ CombatRPG-withControls/scripts/states/MainMenu.tsx
@@ -6,8 +6,12 @@
             }
 
             create() {
-                
                 this.add.sprite(0, 0, "background");
+                var menuButton = <button type="button">Start Game</button>;
+                var target = document.getElementById("main-menu-screen");
+
+                React.render(menuButton, target);
+                $(menuButton).click("#splash-screen"); 
             }
 
         }
