18ec5422a13d6c84367fce0779eba6cc28771cf5
ldurback <luke.durback@gmail.com>
2015-10-28 12:51:30 -0400

    moved map player input into new class

--- CombatRPG-withControls/scripts/States/Maps/EmptyMap.ts
+++ CombatRPG-withControls/scripts/States/Maps/EmptyMap.ts
@@ -6,62 +6,14 @@ namespace CombatRPG {
         export namespace Maps {
             export class EmptyMap extends BaseState {
                 player: Entities.Map.Player;
+                playerInput: PlayerInput;
 
                 loadAssets() {
                     this.load.spritesheet("character", "/assets/images/character.png", 64, 64);
                 }
                 initialize() {
                     this.player = new Entities.Map.Player(this.game, 50, 50);
-
-                    this.setupInput();
-                }
-
-                private setupInput() {
-                    var cursors = this.game.input.keyboard.createCursorKeys();
-
-                    cursors.left.onDown.add(this.onKeyLeftPress, this);
-                    cursors.left.onUp.add(this.onKeyLeftLift, this);
-
-                    cursors.up.onDown.add(this.onKeyUpPress, this);
-                    cursors.up.onUp.add(this.onKeyUpLift, this);
-
-                    cursors.right.onDown.add(this.onKeyRightPress, this);
-                    cursors.right.onUp.add(this.onKeyRightLift, this);
-
-                    cursors.down.onDown.add(this.onKeyDownPress, this);
-                    cursors.down.onUp.add(this.onKeyDownLift, this);
-                }
-
-                private onKeyLeftLift() {
-                    this.player.stopMovingLeft();
-                }
-
-                private onKeyUpLift() {
-                    this.player.stopMovingUp();
-                }
-
-                private onKeyRightLift() {
-                    this.player.stopMovingRight();
-                }
-
-                private onKeyDownLift() {
-                    this.player.stopMovingDown();
-                }
-
-                private onKeyLeftPress() {
-                    this.player.setToMoveLeft();
-                }
-
-                private onKeyUpPress() {
-                    this.player.setToMoveUp();
-                }
-
-                private onKeyRightPress() {
-                    this.player.setToMoveRight();
-                }
-
-                private onKeyDownPress() {
-                    this.player.setToMoveDown();
+                    this.playerInput = new PlayerInput(this.game, this.player);
                 }
             }
         }
---
+++ CombatRPG-withControls/scripts/States/Maps/PlayerInput.ts
@@ -0,0 +1,63 @@
+﻿///<reference path="../../Entities/Map/Player.ts" />
+
+namespace CombatRPG {
+    export namespace States {
+        export namespace Maps {
+            export class PlayerInput {
+                player: Entities.Map.Player;
+                game: Phaser.Game;
+
+                constructor(game: Phaser.Game, player: Entities.Map.Player) {
+                    this.player = player;
+                    this.game = game;
+
+                    var cursors = this.game.input.keyboard.createCursorKeys();
+
+                    cursors.left.onDown.add(this.onKeyLeftPress, this);
+                    cursors.left.onUp.add(this.onKeyLeftLift, this);
+
+                    cursors.up.onDown.add(this.onKeyUpPress, this);
+                    cursors.up.onUp.add(this.onKeyUpLift, this);
+
+                    cursors.right.onDown.add(this.onKeyRightPress, this);
+                    cursors.right.onUp.add(this.onKeyRightLift, this);
+
+                    cursors.down.onDown.add(this.onKeyDownPress, this);
+                    cursors.down.onUp.add(this.onKeyDownLift, this);
+                }
+
+                private onKeyLeftLift() {
+                    this.player.stopMovingLeft();
+                }
+
+                private onKeyUpLift() {
+                    this.player.stopMovingUp();
+                }
+
+                private onKeyRightLift() {
+                    this.player.stopMovingRight();
+                }
+
+                private onKeyDownLift() {
+                    this.player.stopMovingDown();
+                }
+
+                private onKeyLeftPress() {
+                    this.player.setToMoveLeft();
+                }
+
+                private onKeyUpPress() {
+                    this.player.setToMoveUp();
+                }
+
+                private onKeyRightPress() {
+                    this.player.setToMoveRight();
+                }
+
+                private onKeyDownPress() {
+                    this.player.setToMoveDown();
+                }
+            }
+        }
+    }
+}
