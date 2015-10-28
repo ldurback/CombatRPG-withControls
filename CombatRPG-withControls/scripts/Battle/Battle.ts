9cb1ecabadc50962785552b13ea681e20da0797c
Jesse Glover <kohouri@gmail.com>
2015-10-28 02:29:33 -0400

    Changed location of Battle

---
+++ CombatRPG-withControls/scripts/Battle/Battle.ts
@@ -0,0 +1,10 @@
+﻿namespace CombatRPG {
+    export namespace Battle {
+        export abstract class Battle extends Phaser.State {
+            preload() {
+                this.game.state.add("GameOver", States.GameOverScreen);
+
+            }
+        }
+    }
+}
