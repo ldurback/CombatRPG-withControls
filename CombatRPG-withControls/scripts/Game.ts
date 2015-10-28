75f20d15cd63e77dbc90fb94271e1273468072ad
ldurback <luke.durback@gmail.com>
2015-10-28 00:07:13 -0400

    go to boot state

--- CombatRPG-withControls/scripts/Game.ts
+++ CombatRPG-withControls/scripts/Game.ts
@@ -1,7 +1,13 @@
-﻿namespace CombatRPG {
+﻿///<reference path="states/Boot.ts" />
+
+namespace CombatRPG {
     export class Game extends Phaser.Game {
         constructor() {
             super(800, 600, Phaser.AUTO, 'content', null);
+
+            this.state.add("Boot", States.Boot);
+
+            this.state.start("Boot");
         }
     }
 }
