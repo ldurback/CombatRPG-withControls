7f40b676ce1fbbfff460d4bfc881fc63d4c67590
Jesse Glover <kohouri@gmail.com>
2015-10-28 00:36:43 -0400

    changes

--- CombatRPG-withControls/scripts/states/MainMenu.ts
+++ CombatRPG-withControls/scripts/states/MainMenu.ts
@@ -1,12 +1,9 @@
-﻿namespace CombatRPG
-{
-    export namespace States
-    {
-        export class MainMenu extends Phaser.State
-        {
-            loadMenu()
-            {
-
+﻿namespace CombatRPG {
+    export namespace States {
+        export class MainMenu extends Phaser.State {
+            loadMenu() {
+                var background = this.load.image('background', 'assets/images/background.png');
+                this.add.sprite(0, 0, background);
             }
 
         }
