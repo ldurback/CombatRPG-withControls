b08b583e1a0b609a5f1fd66f65d325f504c758cc
ldurback <luke.durback@gmail.com>
2015-10-31 11:46:54 -0400

    made react component to display saved data

---
+++ CombatRPG-withControls/scripts/RComs/SaveLoad/ShowData.tsx
@@ -0,0 +1,32 @@
+﻿namespace CombatRPG {
+    export namespace RComs {
+        export namespace SaveLoad {
+            export interface ShowDataProps extends React.Props<any> {
+                saveData: Utils.SaveData;
+                saveKey: string;
+                onClick?: React.MouseEventHandler;
+                className?: string;
+            }
+
+            export class ShowData extends React.Component<ShowDataProps, {}> {
+                render() {
+                    var onClickEvent: React.MouseEventHandler;
+                    if (this.props.onClick) {
+                        onClickEvent = this.props.onClick;
+                    }
+                    else {
+                        onClickEvent = (e: React.MouseEvent) => {}
+                    }
+
+                    return <div className={this.props.className} onClick={onClickEvent}>
+                        <div>{this.props.saveKey}</div>
+                        <div>{this.props.saveData.return.state}</div>
+                        <div>HP: {this.props.saveData.player.status.currentHP}/{this.props.saveData.player.status.maxHP}</div>
+                        <div>Strength: {this.props.saveData.player.status.strength}</div>
+                        <div>Defense: {this.props.saveData.player.status.defense}</div>
+                        </div>;
+                }
+            }
+        }
+    }
+}
